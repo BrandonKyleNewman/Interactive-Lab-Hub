e19a25c51866c2e289495137258e1538bc9071a9
Brandon Newman <Brandonkylenewman@gmail.com>
2021-09-27 17:28:26 -0400

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -70,23 +70,23 @@ while True:
         draw.rectangle((0, 0, width, height), outline=0, fill=(140, 229, 151))
         current_season = "Spring"
         random_sayings = ["It's sprung brother", "Smell a flower, dude", "Grab an umbrella, brother"]
-        season_image = Image.open('spring.jpg').convert('RGBA')
+        season_image = Image.open('spring.png').convert('RGBA')
     elif str(time.localtime().tm_mon) in ["6","7","8"]:
         draw.rectangle((0, 0, width, height), outline=0, fill=(255, 226, 68))
         current_season = "Summer"
         random_sayings = ["Get some rays, bro", "Suns out, guns out, bro", "Don't forget sunscreen, bro"]
-        season_image = Image.open('summer.jpg').convert('RGBA')
+        season_image = Image.open('summer.png').convert('RGBA')
     elif str(time.localtime().tm_mon) in ["9","10","11"]:
         draw.rectangle((0, 0, width, height), outline=0, fill=(255, 111, 68))
         current_season = "Autumn"
         random_sayings = ["Crunchy leaves, bro", "Pumpkin spice me, bro", "Grab a coat, bro"]
-        season_image = Image.open('./autumn.jpg').convert('RGBA')
+        season_image = Image.open('autumn.png').convert('RGBA')
         draw.image((0,0), season_image)
     else:
         current_season = "Winter"
         draw.rectangle((0, 0, width, height), outline=0, fill=(198, 246, 255))
         random_sayings = ["It's a wonderland, bro", "Build a snowman, bro", "Does it snow in California?"]
-        season_image = Image.open('winter.jpg').convert('RGBA')
+        season_image = Image.open('winter.png').convert('RGBA')
 
     current_hour = time.localtime().tm_hour % 12
     if current_hour == 0:
