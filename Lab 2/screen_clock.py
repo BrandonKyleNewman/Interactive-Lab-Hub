bdee8913069b3084128fbecabe04c818de432f94
Brandon Newman <Brandonkylenewman@gmail.com>
2021-09-27 18:21:00 -0400

    Update screen_clock.py

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -97,7 +97,7 @@ while True:
     y += 24
     draw.text((x,y), "and about " + str(current_hour) + "ish", font=font, flush=True, fill="#5E1560")
     y += 48
-    draw.text((x+75,y), random.choice(random_sayings), font=font_2, flush=True, fill="#5E1560")
+    draw.text((x+100,y), random.choice(random_sayings), font=font_2, flush=True, fill="#5E1560")
     # Display image.
     disp.image(season_image, rotation)
     time.sleep(600)
