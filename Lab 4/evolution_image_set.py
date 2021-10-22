ea596f350c9149dd321745a6302c3c36b3253e5c
Brandon Newman <brandon@macbook-air.lan>
2021-10-21 23:24:05 -0400

    trial time

---
+++ Lab 4/evolution_image_set.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+evolution_image_set = [
+	[
+		'evolution_state_images/state_1/stage-1-0.png',
+		'evolution_state_images/state_1/stage-1-1.png',
+		'evolution_state_images/state_1/stage-1-2.png',
+		'evolution_state_images/state_1/stage-1-3.png'
+	],
+	[
+		'evolution_state_images/state_2/stage-2-0.png',
+		'evolution_state_images/state_2/stage-2-1.png',
+		'evolution_state_images/state_2/stage-2-2.png',
+		'evolution_state_images/state_2/stage-2-3.png'
+	],
+	[
+		'evolution_state_images/state_3/stage-3-0.png',
+		'evolution_state_images/state_3/stage-3-1.png',
+		'evolution_state_images/state_3/stage-3-2.png',
+		'evolution_state_images/state_3/stage-3-3.png'
+	]
+]
+
+create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/evolution_state_images/state_2/stage-2-0.png
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
+ create mode 100644 Lab 4/
--- Lab 4/lab_4.py
+++ Lab 4/lab_4.py
@@ -3,6 +3,7 @@ import subprocess
 import digitalio
 import board
 import random
+import evolution_image_set as evolution_images
 import adafruit_rgb_display.st7789 as st7789
 from PIL import Image, ImageDraw, ImageFont
 from adafruit_apds9960.apds9960 import APDS9960
@@ -70,38 +71,39 @@ backlight.switch_to_output()
 backlight.value = True
 
 EVOLUTION_MAX = 2
-LUM_STEP = 10000
+LUX_STEP = 10000
 
 evolution_state = 0
-total_lum_val = 0
+total_lux_val = 0
 image_count = 0
 
 while True:
-    if total_lum_val >= (evolution_state+1)*LUM_STEP:
+    if total_lux_val >= (evolution_state+1)*LUX_STEP:
         evolution_state += 1
         #evolve
     if evolution_state == EVOLUTION_MAX:
         break
     
-    image_set = evolution_images[evolution_state][image_count%4]
+    curr_image = evolution_images[evolution_state][image_count%4]
     
+    r, g, b, c = apds.color_data
+    curr_lux = colorutility.calculate_lux(r, g, b)
+    total_lux_val += curr_lux
     
     # Draw a black filled box to clear the image.
     # draw.rectangle((0, 0, width, height), outline=0, fill=0)
     y = top
     
-    draw = ImageDraw.Draw(season_image)
-    current_hour = time.localtime().tm_hour % 12
-    if current_hour == 0:
-        current_hour = 12
+    draw = ImageDraw.Draw(curr_image)
+    draw.text((x,y), str(total_lux_val), font=font, flush=True, fill="#5E1560")
             
-    draw.text((x,y), "It's, uh, " + current_season, font=font, flush=True, fill="#5E1560")
-    y += 24
-    draw.text((x,y), "and about " + str(current_hour) + "ish", font=font, flush=True, fill="#5E1560")
-    y += 48
-    draw.text((x+100,y), random.choice(random_sayings), font=font_2, flush=True, fill="#5E1560")
-    y += 12
-    draw.text((x+100,y), random.choice(["dude","bro","brother","my man", "my guy"]), font=font_2, fill="#5E1560")
+    #draw.text((x,y), "It's, uh, " + current_season, font=font, flush=True, fill="#5E1560")
+    #y += 24
+    #draw.text((x,y), "and about " + str(current_hour) + "ish", font=font, flush=True, fill="#5E1560")
+    #y += 48
+    #draw.text((x+100,y), random.choice(random_sayings), font=font_2, flush=True, fill="#5E1560")
+    #y += 12
+    #draw.text((x+100,y), random.choice(["dude","bro","brother","my man", "my guy"]), font=font_2, fill="#5E1560")
     # Display image.
     disp.image(season_image, rotation)
     time.sleep(1)
