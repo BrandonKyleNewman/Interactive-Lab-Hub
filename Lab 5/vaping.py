fc5e7731c6ca83ee74c58e0619e0d17df1b93118
Brandon Newman <brandon@dhcp-vl2052-783.redrover.cornell.edu>
2021-11-08 09:32:59 -0500

    label work

--- Lab 5/vaping.py
+++ Lab 5/vaping.py
@@ -64,8 +64,8 @@ while(True):
     # run the inference
     prediction = model.predict(data)
     print("I think its a:",labels[np.argmax(prediction)])
-    if my_button1.is_button_pressed() == True:
-      print("holy fuck")
+    if labels[np.argmax(prediction)] == 'Not Vaping':
+      print("why cmon stupid")
     if labels[np.argmax(prediction)] == 'Vaping':
       my_button1.LED_on(100)
     else:
